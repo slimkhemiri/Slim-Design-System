961437c3e1afa20397ec905d927864f6c98f5cad
khemiri <khemiri.medslim1@gmail.com>
2026-02-09 10:56:33 +0100

    add storybook

---
+++ tmp-consume/index.ts
@@ -0,0 +1,5 @@
+﻿import { defineCustomElements } from '@slimkhemiri/react-design-system';
+import type { SlimButton } from '@slimkhemiri/react-design-system';
+
+defineCustomElements(window);
+console.log('ok');
